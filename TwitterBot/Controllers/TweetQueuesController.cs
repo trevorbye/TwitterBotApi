f2f95ab8fe9d29b5f7c2c83b39b8d06c25f41968
Trevor Bye <trevor.bye@microsoft.com>
2019-08-20 13:47:44 -0700

    removed redundant imports and old test files

--- TwitterBot/Controllers/TweetQueuesController.cs
+++ TwitterBot/Controllers/TweetQueuesController.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using TwitterBot.Models;
 using TwitterBot.POCOS;
 using System.Security.Claims;
-using System.Data.SqlClient;
+
 
 namespace TwitterBot.Controllers
 {
