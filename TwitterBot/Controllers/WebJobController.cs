9bd8383c56faa2e2637f43c1a6da0d65311bbaf3
Trevor Bye <trevor.bye@microsoft.com>
2019-08-21 11:32:16 -0700

    adding protected auth emthod for webjob

---
+++ TwitterBot/Controllers/WebJobController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TwitterBot.Models;
+using TwitterBot.POCOS;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Security.Claims;
+
+namespace TwitterBot.Controllers
+{
+    public class WebJobController : ApiController
+    {
+        private TwitterBotContext db = new TwitterBotContext();
+        private string _bearerToken = Environment.GetEnvironmentVariable("WEBJOB_AUTH_KEY");
+
+        [Route("api/webjob-fetch-queue")]
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult FetchTweetQueueAndAccounts()
+        {
+            string authToken = Request.Headers.Authorization.Parameter;
+            if (authToken != _bearerToken || authToken == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+    }
+}
