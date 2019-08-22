4014354f4e2c8bf31c6df3af3418aaed74c2d246
Trevor Bye <trevor.bye@microsoft.com>
2019-08-22 16:29:08 -0700

    building webjob config

--- TwitterWebJob/Functions.cs
+++ TwitterWebJob/Functions.cs
@@ -10,11 +10,10 @@ namespace TwitterWebJob
 {
     public class Functions
     {
-        // This function will get triggered/executed when a new message is written 
-        // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        [NoAutomaticTrigger]
+        public static void ProcessTweets()
         {
-            log.WriteLine(message);
+
         }
     }
 }
--- TwitterWebJob/Program.cs
+++ TwitterWebJob/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using Microsoft.Azure.WebJobs;
 
 namespace TwitterWebJob
@@ -11,10 +12,15 @@ namespace TwitterWebJob
     {
         static void Main()
         {
-            while (true)
+            var config = new JobHostConfiguration();
+            if (config.IsDevelopment)
             {
-
+                config.UseDevelopmentSettings();
             }
+
+            ServicePointManager.DefaultConnectionLimit = Int32.MaxValue;
+            var host = new JobHost(config);
+            host.Call(typeof(Functions).GetMethod("ProcessTweets"));
         }
     }
 }
