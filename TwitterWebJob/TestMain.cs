1bd9f090896520f7a7312651163e3dd88e56380b
Trevor Bye <trevor.bye@microsoft.com>
2019-08-23 08:57:53 -0700

    test file for building webjob logic

---
+++ TwitterWebJob/TestMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+
+namespace TwitterWebJob
+{
+    class TestMain
+    {
+        public static void Main(String[] args)
+        {
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            string authString = "Bearer ";
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("https://mstwitterbot.azurewebsites.net/api/webjob-fetch-queue"),
+                Method = HttpMethod.Get,
+                Headers =
+                {
+                    {HttpRequestHeader.Authorization.ToString(), authString}
+                }
+            };
+
+            var response = client.SendAsync(request).Result;
+            WebJobTweetQueueAccountReturnEntity content = response.Content
+                .ReadAsAsync<WebJobTweetQueueAccountReturnEntity>().Result;
+
+            int bp = 0;
+        }
+    }
+}
