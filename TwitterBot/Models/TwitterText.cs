172f249e21dc6ea1f44f945d087a4f3f4da39684
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-02-28 20:56:31 -0800

    text validation for Bobby's new tweets

---
+++ TwitterBot/Models/TwitterText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterBot.Models
+{
+    public class TwitterTextValidationResponse
+    {
+        public string textReturn;
+        public bool isValid;
+    }
+    public class TwitterTextValidationRequest
+    {
+        public string tweetText;
+    }
+}
