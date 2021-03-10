788b97446e5042e91313185eb7f81570a1e92bb0
Trevor Bye <trevor.bye@microsoft.com>
2021-03-10 13:56:15 -0800

    adding custom auth provider

---
+++ TwitterBot/App_Start/CustomAuthProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+
+
+namespace TwitterBot.App_Start
+{
+    public class CustomAuthProvider : OAuthBearerAuthenticationProvider
+    {
+        public override Task ValidateIdentity(OAuthValidateIdentityContext context)
+        {
+            List<Claim> claims = context.Ticket.Identity.Claims.ToList();
+            var userName = claims[9].Value;
+            if (!userName.EndsWith("@microsoft.com"))
+            {
+                context.Rejected();
+            }
+            return base.ValidateIdentity(context);
+        }
+    }
+}
